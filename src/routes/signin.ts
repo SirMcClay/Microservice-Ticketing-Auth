c1e6452265c97bf23f7e22f9ed0bd119bc43a228
sirmcclay <mcclay.design@gmail.com>
2020-12-11 12:56:05 -0300

    Delete validationResult import from signin.ts

--- src/routes/signin.ts
+++ src/routes/signin.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
+import { body } from 'express-validator';
 
 import { validateRequest } from '../middlewares/validate-request';
 
